7a17f7de52c1daf20fa93053e9e553a7cf3fcd1a
branham551691 <91641579+branham551691@users.noreply.github.com>
2021-11-03 11:03:32 -0700

    Update a121_catch_a_turtle.py

--- a121_catch_a_turtle.py
+++ a121_catch_a_turtle.py
@@ -1,12 +1,24 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
-import turtle as trtl
+import turtle as t
 import random as rand
-t=trtl
 #-----game configuration----
 shape = "circle"
 size = "2"
-color = "blue"
+color = "lavender"
+font_setup = ("Arial", 20, "normal")
+score = 0
+
+#-----counter-----
+counter_interval = 1000
+timer_up = False
+timer = 30
+count = t.Turtle()
+count.hideturtle()
+count.penup()
+count.goto(150,200)
+count.pendown()
+
 
 #-----initialize turtle-----
 t.begin_fill()
@@ -14,20 +26,48 @@ t.shape(shape)
 t.shapesize(int(size))
 t.fillcolor(color)
 t.end_fill()
+score_writer = t.Turtle()
+score_writer.hideturtle()
+score_writer.penup()
+score_writer.goto(150,150) 
+score_writer.pendown()
+score_writer.hideturtle()
 
 #-----game functions--------
-def spot_clicked (x, y):
-  change_position(x, y)
-t.onclick(spot_clicked)
-
-def change_position(x, y):
-  t.penup()
-  new_xpos = rand.randint(-150, 150)
-  new_ypos = rand.randint(-150, 150)
-  rand.randint(-200, 200)
-  t.goto(new_xpos, new_ypos)
-
-
-#-----events----------------
+def update_score():
+    global score 
+    score += 1
+    score_writer.clear()
+    score_writer.write(score, font = font_setup)
+def spot_clicked(x,y):
+    global timer_up
+    if (not timer_up):
+        update_score()
+        change_position()
+    else:
+        t.hideturtle()
+        update_score()
+def change_position():
+    t.penup()
+    t.hideturtle()
+    new_ypos = rand.randint(-250,250)
+    new_xpos = rand.randint(-250,250)
+    t.goto(new_xpos,new_ypos)
+    t.pendown()
+    t.showturtle()
+def countdown():
+    global timer, timer_up
+    count.clear()
+    if timer <= 0:
+        count.write("time's up!", font = font_setup)
+        timer_up = True
+    else:
+        count.write("timer: " + str(timer), font = font_setup)
+        timer -= 1
+        count.getscreen().ontimer(countdown, counter_interval)
+ 
+#-----end-----
 wn = t.Screen()
+t.onclick(spot_clicked)
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
