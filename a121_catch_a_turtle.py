02325fef506449f2730bc714daba70f854284737
branham551691 <91641579+branham551691@users.noreply.github.com>
2021-11-10 10:55:32 -0800

    Update a121_catch_a_turtle.py

--- a121_catch_a_turtle.py
+++ a121_catch_a_turtle.py
@@ -2,31 +2,52 @@
 #-----import statements-----
 import turtle as t
 import random as rand
+'''''
+import leaderboard as lb
+'''''
 #-----game configuration----
 shape = "circle"
 size = "2"
 color = "lavender"
 font_setup = ("Arial", 20, "normal")
 score = 0
-
 #-----background-----
 bg_color = "purple"
 t.bgcolor(bg_color)
-
 #-----list of colors-----
-colors = ['pink', 'blue', 'green', 'beige', 'purple', 'yellow']
+colors = ["pale green", "sky blue", "pink", "yellow", "beige"]
 #-----list of sizes-----
 sizes = [1, 1.5, 2, 2.5]
 #-----counter-----
 counter_interval = 1000
 timer_up = False
-timer = 30
+timer = 5
 count = t.Turtle()
 count.hideturtle()
 count.penup()
 count.goto(150,150)
 count.pendown()
-
+#-----leaderboard-----
+leaderboard_file_name = "a122_leaderboard.txt"
+leader_names_list = []
+leader_scores_list = []
+player_name = input("please enter your name:  ")
+#-----manages the leaderboard for top 5 scorers-----
+def manage_leaderboard():
+  global leader_scores_list
+  global leader_names_list
+  global score
+  global spot
+  '''''
+  #-----load all the leaderboard records into the lists-----
+  lb.load_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list)
+  #-----TODO-----
+  if (len(leader_scores_list) < 5 or score > leader_scores_list[4]):
+    lb.update_leaderboard(leaderboard_file_name, leader_names_list, leader_scores_list, player_name, score)
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, True, spot_clicked, score)
+  else:
+    lb.draw_leaderboard(leader_names_list, leader_scores_list, False, spot_clicked, score)
+    '''''
 #-----initialize turtle-----
 t.begin_fill()
 t.shape(shape)
@@ -39,7 +60,8 @@ score_writer.penup()
 score_writer.goto(150,100) 
 score_writer.pendown()
 score_writer.hideturtle()
-
+spot = t.Turtle()
+spot.color("purple")
 #-----game functions--------
 def update_score():
     global score 
@@ -53,6 +75,13 @@ def spot_clicked(x,y):
         change_position()
     else:
         t.hideturtle()
+def resize():
+  sizes = [.5, 1, 1.5, 2]
+  spot.shapesize(rand.choice(sizes))
+def leave_a_mark():
+  spot.fillcolor(rand.choice(colors[1:]))
+  spot.stamp()
+  spot.fillcolor(colors[0])
 def change_position():
     t.penup()
     t.hideturtle()
@@ -67,6 +96,7 @@ def countdown():
     if timer <= 0:
         count.write("time's up!", font = font_setup)
         timer_up = True
+        manage_leaderboard()
     else:
         count.write("timer: " + str(timer), font = font_setup)
         timer -= 1
